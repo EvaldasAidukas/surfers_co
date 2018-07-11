e7afcf99b44cc88a8e5276ce52ad3d4c67b316d8
Evaldas <evaldas.aidukas@gmail.com>
2018-07-11 12:36:57 +0300

    add carousel to top

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
 
 
-    console.log("js working");
     // $(".owl-carousel").owlCarousel();
-    var owl = $('.owl-carousel').owlCarousel({
+    var owl = $('.team-mates').owlCarousel({
         loop:false,
         //margin:200,
         autowidth:false,
@@ -34,14 +33,35 @@ $(document).ready(function(){
         owl.trigger('next.owl.carousel');
     });
 
+    $('.burger').click(function(){
+        $('.nav-list').toggleClass('show');
+        //$('.social-icons').toggleClass('show');
+    });
+
+    var boards = $('.board-gallery').owlCarousel({
+        loop:false,
+        autowidth:true,
+        nav:false,
+        dots:true,
+        singleItem: true,
+        pagination: true,
+        dotData: true,
+        dotsData: true,
+        responsive:{
+            0:{
+                items:1
+            }
+        }
+    });
+
 });
 
-function myFunction() {
-    var x = document.getElementsByClassName("nav-bar")[0];
-    if (x.className === "nav-bar flex") {
-        console.log("called");
-        x.className += " responsive";
-    } else {
-        x.className = "nav-bar flex";
-    }
-}
+// function myFunction() {
+//     var x = document.getElementsByClassName("nav-bar")[0];
+//     if (x.className === "nav-bar flex") {
+//         console.log("called");
+//         x.className += " responsive";
+//     } else {
+//         x.className = "nav-bar flex";
+//     }
+// }
